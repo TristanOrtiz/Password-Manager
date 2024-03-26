52160d7e4a857c821755b976e7a4c01a07e158bc
Tristan Ortiz <tristanortiz75@gmail.com>
2024-03-26 01:07:16 -0500

    Routes implemented and pages created

---
+++ client/src/pages/NoPage.js
@@ -0,0 +1,9 @@
+
+
+export default function NoPage() {
+    return(
+        <>
+            <h1>Error 404: Page could not be found</h1>
+        </>
+    )
+}
--- flask-server/app.py
+++ flask-server/app.py
@@ -1,4 +1,9 @@
+import os
+import psycopg2
+from dotenv import load_dotenv
 from flask import Flask
+
+load_dotenv()
 
 app = Flask(__name__)
 
